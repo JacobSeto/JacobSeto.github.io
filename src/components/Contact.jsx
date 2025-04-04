41433582ea6673a3e8b2cec2a28066846aa8f955
JacobSeto <jacobtuxseto1234@gmail.com>
2025-04-04 16:46:33 -0400

    Revert "Fixed images not loading"
    This reverts commit ac953acb156f7fbc2fbbec8a42dbe85b2340a77a.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,14 +6,14 @@ export default function Contact() {
           <div className="contact-left-title">
             <h2>Get in touch</h2><hr />
           </div>
-          <input type="hidden" name="access_key" value="41a36971-7dd7-4d39-9c74-51e7f9c8d64e" />
-          <input type="text" name="name" placeholder="Name" className="contact-inputs" required />
-          <input type="email" name="email" placeholder="Email" className="contact-inputs" required />
-          <textarea name="message" placeholder="Message..." className="contact-inputs" required />
-          <button type="submit">Submit <img src="/src/assets/images/arrow_icon.png" alt="" /></button>
+          <input type="hidden" name="access_key" value="41a36971-7dd7-4d39-9c74-51e7f9c8d64e"/>
+          <input type="text" name="name" placeholder="Name" className="contact-inputs" required/>
+          <input type="email" name="email" placeholder="Email" className="contact-inputs" required/>
+          <textarea name="message" placeholder="Message..." className="contact-inputs" required/>
+          <button type="submit">Submit <img src="/images/arrow_icon.png" alt=""/></button>
         </form>
         <div className="contact-right">
-          <img src="/src/assets/images/text bubble float.png" alt="" />
+          <img src="/images/text bubble float.png" alt=""/>
         </div>
       </div>
     </section>
