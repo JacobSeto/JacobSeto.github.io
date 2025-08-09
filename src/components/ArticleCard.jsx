d449a1d71d8068d97ffc0eada07158784f60b743
JacobSeto <jacobtuxseto1234@gmail.com>
2025-08-09 03:01:43 -0400

    Fixed article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,13 +2,9 @@ export default function ArticleCard() {
   return (
     <div className="article-card">
       <div className="article-left">
-        <p>
-          Read my article about Cornell&apos;s{" "}
           <a href="https://news.cornell.edu/stories/2025/03/dgas-first-game-jam-gives-game-developers-chance-level" target="_blank" rel="noreferrer">
-            first large-scale Game Jam
+            Featured Article
           </a>
-          !
-        </p>
       </div>
       <div className="article-right">
         <img src="/images/Featured Article.webp" alt="Featured article promo" />
