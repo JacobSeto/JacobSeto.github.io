67c52d1ce4ffad7259162e4cb64a1c21365e3d3d
JacobSeto <jacobtuxseto1234@gmail.com>
2025-08-08 23:44:18 -0400

    Update vite.config.js

--- jacobseto-site/vite.config.js
+++ jacobseto-site/vite.config.js
@@ -1,8 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/jacobseto-site/', // <-- IMPORTANT for project Pages
-})
+export default defineConfig({ plugins: [react()] })
